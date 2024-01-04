0f1c6fa1e6be0a113cda22da999e320ca09db892
donfortune <donfortunet.df@gmail.com>
2024-01-04 18:34:35 +0100

    explored decorator. looking at property and setter decorators

--- DAY_3/test_add.py
+++ DAY_3/test_add.py
@@ -3,7 +3,11 @@ from add import add
 
 class test_case(unittest.TestCase):
     def test_add(self):
-        self.assertTrue(add(1, 5), 6)
+        self.assertEqual(add(1, 5), 6)
+    def test_add2(self):
+        self.assertEqual(add(1, 7), 6)
+        
+        
 
         
 
